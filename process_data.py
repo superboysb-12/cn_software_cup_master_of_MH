d8fed4d6fc27a1ed9e168cc4db5048e823850564
CAD1311 <y927053212@163.com>
2024-05-24 14:24:05 +0800

    alter process_data.py

--- process_data.py
+++ process_data.py
@@ -1,20 +1,25 @@
-import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
+import pandas as pd
 
 def process_permissions(df):
     df['permissions'] = df['permissions'].apply(eval)
 
+    df['official_permissions'] = df['permissions'].apply(lambda x: [i for i in x if i.startswith('android.permission.')])
+    df['customer_permissions'] = df['permissions'].apply(lambda x: [i for i in x if not i.startswith('android.permission.')])
+
+    df['has_customer_permission'] = df['customer_permissions'].apply(lambda x: 0 if len(x) == 0 else 1)
+
     mlb = MultiLabelBinarizer()
-    permissions_binarized = mlb.fit_transform(df['permissions'])
+    permissions_binarized = mlb.fit_transform(df['official_permissions'])
 
     permissions_df = pd.DataFrame(permissions_binarized, columns=mlb.classes_)
 
-    df = pd.concat([df.drop('permissions', axis=1), permissions_df], axis=1)
+    df = pd.concat([df.drop(['permissions', 'official_permissions'], axis=1), permissions_df], axis=1)
 
     return df
 
-df = pd.read_csv('../dataset/merged.csv', encoding='gbk')
+df = pd.read_csv('../dataset/merged.csv', encoding='latin1')
 
 df = process_permissions(df)
 
-df.to_csv('../dataset/new_merged.csv', index=False,)
+df.to_csv('../dataset/new_merged.csv', index=False)
