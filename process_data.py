e36a9c196a2b6f348f06e9d64641ea0f24459c17
CAD1311 <y927053212@163.com>
2024-05-26 16:21:46 +0800

    添加transformer 整理文件

--- process_data.py
+++ process_data.py
@@ -1,5 +1,8 @@
 from sklearn.preprocessing import MultiLabelBinarizer
 import pandas as pd
+from sklearn.manifold import Isomap
+import numpy as np
+import random
 
 def process_permissions(df):
     df['permissions'] = df['permissions'].apply(eval)
@@ -18,8 +21,47 @@ def process_permissions(df):
 
     return df
 
-df = pd.read_csv('../dataset/merged.csv', encoding='gbk')
+def transform_data(data,target_dimension : int): # -> ndarray
+    #数据量与维度
+    n_samples,n_features=len(data),len(data[0])
+    #创建模型
+    isomap = Isomap(n_components=target_dimension)
+    # 对数据进行拟合和转换
+    transformed_data = isomap.fit_transform(data)
+    return transformed_data
 
-df = process_permissions(df)
+def strengthen_data(file_path : str = 'None',
+                    target_path : str = '',
+                    target_num : int = 100,
+                    data = None,target_name : str = "strengthened_data",
+                    merge : bool = True):
 
-df.to_csv('../dataset/new_merged.csv', index=False,encoding='gbk')
+    #file_path和 data至少一个输入且优先使用file_path
+    if file_path !='None':
+        data = pd.read_csv(file_path, encoding="gbk")
+    else:
+        if data is None:
+            print('ERROR')
+            return
+    r,c = len(data),len(data.columns)
+    columns = [data.columns[i] for i in range(c)]
+    new = pd.DataFrame(columns =columns)
+    #数据混合产生新数据
+    for _ in range(target_num):
+        #产生r个随机数
+        random_index = [random.randint(1, r-1) for _ in range(c)]
+        #随机组合产生新数据
+        new_row = pd.Series([data[columns[i]][random_index[i]] for i in range(c)],index = columns)
+        new = new._append(new_row,ignore_index=True)
+
+    if merge:
+        new = pd.concat([new,data], ignore_index=True)
+    new.to_csv(target_path+'\\'+target_name+'.csv', index=False,encoding='gbk')
+
+    return new
+
+#df = pd.read_csv('../dataset/merged.csv', encoding='gbk')
+
+#df = process_permissions(df)
+
+#df.to_csv('../dataset/new_merged.csv', index=False,encoding='gbk')
