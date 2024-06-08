bc4d6a932c901e869ac195eb8156ad03061b1117
tatoba <54407436+superboysb-12@users.noreply.github.com>
2024-06-08 12:29:20 +0800

    下载工具

--- utils_download.py
+++ utils_download.py
@@ -4,6 +4,87 @@ import os
 import threading
 import requests
 import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+curdir = os.getcwd()  # 获取当前路径current work directory
+
+data_dir = os.path.join(curdir, "data")
+
+# 创建文件夹
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+
+def check_for_apk(directory=data_dir):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.apk'):
+                return True
+    return False
+
+
+def get_redirected_url(url, page_url):
+    try:
+        if not url.startswith('http'):
+            url = page_url + url  # 拼接成绝对URL
+        response = requests.head(url, allow_redirects=True)
+        redirected_url = response.url
+        return redirected_url
+    except requests.RequestException as e:
+        print("Error:", e)
+        return None
+
+
+def get_static_links(url):
+    # 发送GET请求获取页面内容
+    response = requests.get(url)
+    # 使用BeautifulSoup解析HTML
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # 找到所有的<a>标签
+    links = soup.find_all('a')
+    # 提取链接
+    all_links = [link.get('href') for link in links if link.get('href')]
+
+    return all_links
+
+
+def get_dynamic_links(url):
+    # 使用Selenium模拟浏览器行为
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')  # 无头模式
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    # 提取动态加载后的链接
+    dynamic_links = driver.execute_script(
+        'return [].map.call(document.querySelectorAll("a"), function(link) { return link.href; })')
+
+    driver.quit()
+
+    return dynamic_links
+
+
+def get_all_links(url):
+    static_links = get_static_links(url)
+    dynamic_links = get_dynamic_links(url)
+    all_links = static_links + dynamic_links  # 将动态链接添加到列表中
+    apk_links = []
+    for link in all_links:
+        if is_apk_url(link):
+            apk_links.append(link)
+        else:
+            redirected_url = get_redirected_url(link, url)  # 传递page_url参数
+            if redirected_url and is_apk_url(redirected_url):
+                apk_links.append(redirected_url)
+
+    return apk_links
+
+
+def is_apk_url(url):
+    return url.endswith('.apk')
+
 
 def get_qrcode(image_path):
     img = cv2.imread(image_path)
@@ -11,6 +92,7 @@ def get_qrcode(image_path):
     for obj in decoded_objects:
         if obj.type == 'QRCODE':
             return obj.data.decode('utf-8')
+
 
 def generate_header():
     current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -21,45 +103,56 @@ def generate_header():
     return header
 
 
-curdir = os.getcwd()  # 获取当前路径current work directory
-
-# 创建文件夹
-if not os.path.exists("downloaded_apk"):
-    os.system("mkdir downloaded_apk")
-
-
 def download_single_apk(apk_url):
-    '''下载单个apk文件'''
     # print(apk_url)
-    current_time = datetime.datetime.now()
-    current_time = current_time.strftime('%Y-%m-%d_%H_%M_%S')
-    save_path = os.path.join(curdir, "downloaded_apk", "%s.apk" % current_time)
-    if not os.path.exists(save_path):  # 避免二次下载
-        print("Downloading  %s" % (save_path))
-        try:
-            r = requests.get(apk_url, headers=generate_header(), allow_redirects=True, timeout=180000)  # 发起requests下载请求
-            status_code = r.status_code
-            if (status_code == 200 or status_code == 206):
-                with open(save_path, "wb") as hf:
-                    hf.write(r.content)
-        except:
-            print("Error, can not download apk" )
-    else:
-        print("%s downloaded already!" % save_path)
+    save_path = os.path.join(curdir, "data", "%s" % os.path.basename(apk_url))
+    try:
+        r = requests.get(apk_url, headers=generate_header(), allow_redirects=True, timeout=180)  # 发起requests下载请求
+        status_code = r.status_code
+        if (status_code == 200 or status_code == 206):
+            with open(save_path, "wb") as hf:
+                hf.write(r.content)
+        print("正在下载中！")
+        return 0
 
+    except:
+        print("发生错误,无法下载APK")
+        return -1
+
+
+def download_apk(method_code, url, qrcode):
+    if method_code == 1:
+        download_single_apk(url)
+    elif method_code == 2:
+        urls = get_qrcode(qrcode)
+        for url_a in urls:
+            if is_apk_url(url_a):
+                download_single_apk(url_a)
+            else:
+                urls_a = get_all_links(url_a)
+                for apk_url in urls_a:
+                    download_single_apk(apk_url)
+    elif method_code == 3:
+        urls_a = get_all_links(url)
+
+        for apk_url in urls_a:
+            download_single_apk(apk_url)
+    else:
+        pass
+    return check_for_apk()
 
 ###批量下载的线程
-class DownLoadThread(threading.Thread):
-    def __init__(self, q_job):
-        self._q_job = q_job
-        threading.Thread.__init__(self)
-
-    def run(self):
-        while True:
-            if self._q_job.qsize() > 0:
-                download_single_apk(self._q_job.get())  # 这是10个线程都运行这个下载函数
-            else:
-                break
+# class DownLoadThread(threading.Thread):
+#     def __init__(self, q_job):
+#         self._q_job = q_job
+#         threading.Thread.__init__(self)
+#
+#     def run(self):
+#         while True:
+#             if self._q_job.qsize() > 0:
+#                 download_single_apk(self._q_job.get())  # 这是10个线程都运行这个下载函数
+#             else:
+#                 break
 
 
 # if __name__ == '__main__':
