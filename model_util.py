9ceb70ed7d97e8ef59f63ce4ef81f8414bf3869d
tatoba <54407436+superboysb-12@users.noreply.github.com>
2024-06-08 21:44:18 +0800

    模型小工具就这样了

--- model_util.py
+++ model_util.py
@@ -1,33 +1,34 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-# MLP Model Definition
+# MLP模型定义
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, input_size):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
+        self.layer1 = nn.Linear(input_size, 64)
+        self.layer2 = nn.Linear(64, 32)
+        self.output = nn.Linear(32, 2)  # 二分类任务
 
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = self.output(x)
+        return x
 
-class predictor():
-    def __init__(self, pth_path="checkpoint_epoch_100.pth"):
-        self.model = MLP(365, 50, 2)
+
+class Predictor:
+    def __init__(self, input_size=366, pth_path="mlp_model.pth"):
+        self.model = MLP(input_size)
         self.checkpoint = torch.load(pth_path)
         self.model.load_state_dict(self.checkpoint['model_state_dict'])
+        self.scaler = self.checkpoint['scaler']
+        self.model.eval()
 
-
-    def predict(self,sample):
+    def predict(self, sample):
         with torch.no_grad():
-            sample = sample.iloc[:, [2] + list(range(6, sample.shape[1] - 1))].values
+            sample = self.scaler.transform([sample])
             sample = torch.tensor(sample, dtype=torch.float32)
             output = self.model(sample)
-            predicted_class = torch.argmax(output).item()
+            predicted_class = torch.argmax(output, dim=1).item()
         return predicted_class
-
-
